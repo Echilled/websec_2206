955995a3d772df99c9e6e7a4241c36de980c113d
Eugene Chew <echew140899@gmail.com>
2022-06-11 13:12:00 +0800

    compare hash

--- hellomofos.py
+++ hellomofos.py
@@ -1,65 +1,48 @@
-import Tkinter
-import Tkinter as tk
-# Import requests (to download the page)
-import requests
 
-# Import BeautifulSoup (to parse what we download)
-from bs4 import BeautifulSoup
+import sys
+import hashlib
 
-# Import Time (to add a delay between the times the scape runs)
-import time
+def check_hash(file):
+    # A arbitrary (but fixed) buffer
+    # size (change accordingly)
+    # 65536 = 65536 bytes = 64 kilobytes
+    BUF_SIZE = 65536
 
-# Import smtplib (to allow us to email)
-import smtplib
+    # Initializing the sha256() method
+    sha256 = hashlib.sha256()
+
+    # Opening the file provided as
+    # the first commandline argument
+    with open(file, 'rb') as f:
+        while True:
+            # reading data = BUF_SIZE from
+            # the file and saving it in a
+            # variable
+            data = f.read(BUF_SIZE)
+
+            # True if eof = 1
+            if not data:
+                break
+
+            # Passing that data to that sh256 hash
+            # function (updating the function with
+            # that data)
+            sha256.update(data)
+
+    # sha256.hexdigest() hashes all the input
+    # data passed to the sha256() via sha256.update()
+    # Acts as a finalize method, after which
+    # all the input data gets hashed hexdigest()
+    # hashes the data, and returns the output
+    # in hexadecimal format
+    return sha256.hexdigest()
+
+
 
 def main():
-    # This is a pretty simple script. The script downloads the homepage of VentureBeat, and if it finds some text, emails me.
-    # If it does not find some text, it waits 60 seconds and downloads the homepage again.
 
-    # while this is true (it is true by default),
-    while True:
-        # set the url as VentureBeat,
-        url = "http://Google.com/"
-        # set the headers like we are a browser,
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-        # download the homepage
-        response = requests.get(url, headers=headers)
-        # parse the downloaded homepage and grab all text, then,
-        soup = BeautifulSoup(response.text, "lxml")
+    print("hello world")
 
-        # if the number of times the word "Google" occurs on the page is less than 1,
-        if str(soup).find("Google") == -1:
-            # wait 60 seconds,
-            time.sleep(60)
-            # continue with the script,
-            continue
 
-        # but if the word "Google" occurs any other number of times,
-        else:
-            # create an email message with just a subject line,
-            msg = 'Subject: This is Chris\'s script talking, CHECK GOOGLE!'
-            # set the 'from' address,
-            fromaddr = 'YOUR_EMAIL_ADDRESS'
-            # set the 'to' addresses,
-            toaddrs = ['AN_EMAIL_ADDRESS', 'A_SECOND_EMAIL_ADDRESS', 'A_THIRD_EMAIL_ADDRESS']
-
-            # setup the email server,
-            # server = smtplib.SMTP('smtp.gmail.com', 587)
-            # server.starttls()
-            # add my account login name and password,
-            # server.login("YOUR_EMAIL_ADDRESS", "YOUR_PASSWORD")
-
-            # Print the email's contents
-            print('From: ' + fromaddr)
-            print('To: ' + str(toaddrs))
-            print('Message: ' + msg)
-
-            # send the email
-            # server.sendmail(fromaddr, toaddrs, msg)
-            # disconnect from the server
-            # server.quit()
-
-            break
 
 main()
