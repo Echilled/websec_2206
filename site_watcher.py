ff6bd319c25a855f52824f1ba404ee17551162c5
Eugene Chew <echew140899@gmail.com>
2022-06-18 19:42:38 +0800

    added Testing files

--- site_watcher.py
+++ site_watcher.py
@@ -1,11 +1,11 @@
 import filecmp
 import hashlib
 import os
-from urllib.parse import urlparse
-
 from bs4 import BeautifulSoup
 import requests
 from fake_useragent import UserAgent
+import urllib.request
+
 
 
 class Site_watcher():
@@ -22,7 +22,7 @@ class Site_watcher():
         # SHA256 hash of source code
         sha256_new = hashlib.sha256(str(soup).encode()).hexdigest()
 
-        # check if directory exists
+        # check if directory and previous hash exists
         if os.path.isdir(get_hostname(self._url)) is False:
             os.mkdir(get_hostname(self._url))
             print('First time hash storing')
@@ -32,6 +32,14 @@ class Site_watcher():
             with open(get_hostname(self._url)+'site_256_new.txt', 'w') as f:
                 f.write(sha256_new)
             return sha256_new
+
+
+def print_html(URL):
+    fp = urllib.request.urlopen(URL)
+    mybytes = fp.read()
+    mystr = mybytes.decode("utf8")
+    fp.close()
+    print(mystr)
 
 
 def get_hostname(url):
@@ -51,13 +59,15 @@ def compare_hash(URL):
                 print('Not Updated', SHA256_code)
             else:
                 print("site has changed")
-
+                # notify users
+                os.remove(get_hostname(URL)+'site_256.txt')
+                os.rename(get_hostname(URL)+'site_256_new.txt', get_hostname(URL)+'site_256.txt')
 
 
 def main():
     URL = 'http://www.beefychilled.tk/'
     compare_hash(URL)
-
+    # print_html(URL)
 
 if __name__ == '__main__':
     main()
