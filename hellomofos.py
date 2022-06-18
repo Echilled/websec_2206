4d0edec6b8683369a2ba86371fd66d77ff697efd
Eugene Chew <echew140899@gmail.com>
2022-06-18 11:57:34 +0800

    added some stuff

--- hellomofos.py
+++ hellomofos.py
@@ -1,6 +1,8 @@
 import filecmp
 import hashlib
 import os
+from urllib.parse import urlparse
+
 from bs4 import BeautifulSoup
 import requests
 from fake_useragent import UserAgent
@@ -17,35 +19,45 @@ class Site_watcher():
         response = requests.get(self._url, headers={'User-Agent': ua.random})
         soup = BeautifulSoup(response.text, "html.parser")
 
-        # MD5 hash of source code
+        # SHA256 hash of source code
         sha256_new = hashlib.sha256(str(soup).encode()).hexdigest()
-        # Save file
-        with open('site_256_new.txt', 'w') as f:
-            f.write(sha256_new)
-        return sha256_new
+
+        # check if directory exists
+        if os.path.isdir(get_hostname(self._url)) is False:
+            os.mkdir(get_hostname(self._url))
+            print('First time hash storing')
+            with open(get_hostname(self._url) + 'site_256.txt', 'w') as f:
+                f.write(sha256_new)
+        else:
+            with open(get_hostname(self._url)+'site_256_new.txt', 'w') as f:
+                f.write(sha256_new)
+            return sha256_new
 
 
-# def compare_hash(file1, file2):
-#     file1_hash = check_hash(file1)
-#     file2_hash = check_hash(file2)
-#     if file1_hash == file2_hash:
-#         return True
-#     else:
-#         return False
+def get_hostname(url):
+    if "http://www." in url:
+        return url.replace("http://www.","")
+
+    if "https://www." in url:
+        return url.replace("https://www.","")
 
 
-def main():
-    URLs = []
-    URL = 'http://www.beefychilled.tk/'
+def compare_hash(URL):
     # get latest hash
     SHA256_code = Site_watcher(URL).SHA256_code()
-    if os.path.isfile('site_256.txt'):
-            if filecmp.cmp('site_256.txt', 'site_256_new.txt'):
-                    print('Not Updated', SHA256_code)
+
+    if os.path.isfile(get_hostname(URL)+'site_256.txt') and os.path.isfile(get_hostname(URL)+'site_256_new.txt'):
+            if filecmp.cmp('site_256.txt', 'site_256.txt'):
+                print('Not Updated', SHA256_code)
             else:
                 print("site has changed")
 
 
+
+def main():
+    URL = 'http://www.beefychilled.tk/'
+    compare_hash(URL)
+
+
 if __name__ == '__main__':
     main()
-    
