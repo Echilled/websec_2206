909571fccd6b3ed43eeede7f4214e0657f7022a6
Eugene Chew <echew140899@gmail.com>
2022-07-04 10:38:04 +0800

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,9 +1,13 @@
 
 
+import tkinter as tk
 
 
 def main():
-    pass
+    parent_window = tk.Tk()  # landing page
+    txt = tk.Label(parent_window, text="Web Mon")
+    txt.pack()
+    parent_window.mainloop()
 
 
 if __name__ == '__main__':
