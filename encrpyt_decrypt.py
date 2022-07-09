53140a502f214967438e793e3aa077412a544db7
Eugene Chew <echew140899@gmail.com>
2022-07-09 12:09:38 +0800

    Create encrpyt_decrypt.py

---
+++ encrpyt_decrypt.py
@@ -0,0 +1,20 @@
+import cryptography
+from cryptography.fernet import Fernet
+import secrets
+import base64
+import getpass
+
+
+def encrypt(filename, key):
+    """
+    Given a filename (str) and key (bytes), it encrypts the file and write it
+    """
+    f = Fernet(key)
+    with open(filename, "rb") as file:
+        # read all file data
+        file_data = file.read()
+    # encrypt data
+    encrypted_data = f.encrypt(file_data)
+    # write the encrypted file
+    with open(filename, "wb") as file:
+        file.write(encrypted_data)
