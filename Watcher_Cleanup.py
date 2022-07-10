9ca55972059e8ffa889d1d29965d123f62b3bb5f
Eugene Chew <echew140899@gmail.com>
2022-07-10 19:41:28 +0800

    stats for number of times website change

--- Watcher_Cleanup.py
+++ Watcher_Cleanup.py
@@ -14,6 +14,7 @@ import time
 DRIVER = webdriver.Chrome("chromedriver.exe")
 SITE = ["https://time.gov/", "https://www.ledr.com/colours/white.htm", "http://randomcolour.com/"]  # need test with same domain diff dir
 INDEX = {}
+times_url_change_dict = {}
 DOM_CHANGES = {}
 APP_PASSWORD = 'happymother123'
 
@@ -65,15 +66,37 @@ def format_title(title):
     return title
 
 
+def index_change_history():
+    with open("WebHash.Json", "r") as file:
+        try:
+            data = json.load(file)
+            for url, property in data['URLs'].items():
+                times_url_change_dict[url] = property['properties']['number of times URL content change']
+                # print(properties)
+        except Exception as e:
+            print(e)
+
+
+def update_change_history():
+    index_change_history()
+    print(times_url_change_dict)
+    for key in times_url_change_dict.keys():
+        if key in DOM_CHANGES:
+            times_url_change_dict[key] = times_url_change_dict.get(key, 0) + 1
+    print(times_url_change_dict)
+
+
 def archive_updater():
     # with open("WebHash.txt", "w+") as wf:
     print("Updating archive")
     # wf.writelines("\n".join(','.join((key,val)) for (key,val) in INDEX.items()))
     JSON_values = []   # Archive web page hash
     temp_dict = {'URLs': {}}
+    update_change_history()
     for key, val in INDEX.items():
         now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        JSON_tuple = (key, val, now)
+        changes_number = times_url_change_dict[key]
+        JSON_tuple = (key, val, now, changes_number)
         JSON_values.append(JSON_tuple)
     for val in JSON_values:
         JSON_dict = json_construct(*val)
@@ -88,9 +111,9 @@ def archive_updater():
         os.rename(new, page_title + ".html")
 
 
-def json_construct(id, hash, date):
+def json_construct(id, hash, date, times_it_changed):
     website_dic = {id: {'properties': {}}}
-    values = [{'hash': hash}, {'archival_date': date}]
+    values = [{'hash': hash}, {'archival_date': date}, {'number of times URL content change': times_it_changed}]
     for val in values:
         website_dic[id]['properties'].update(val)
     return website_dic
@@ -169,7 +192,7 @@ def page_changes_listing(changes_dict):
             print(key)
         print('Here are the changes:')
         for key, value in DOM_CHANGES.items():
-            print(key)
+            print('\n', key)
             print("Original content:")
             print(value[0])
             print("Changed content:")
@@ -239,6 +262,7 @@ def main():
     # print(clean_urls(list((url_crawled.crawled_urls))))
     # key = encrpyt_decrypt.derive_key(encrpyt_decrypt.generate_salt(), APP_PASSWORD)
     # base64.urlsafe_b64encode(key)
+    # update_change_history()
     periodic_check(30)
 
 
